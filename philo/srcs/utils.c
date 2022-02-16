af3ca63283c01fd469ae0ae3ff104ebd50254045
DimiOui <omygoditsdimi@gmail.com>
2022-02-16 11:29:17 +0100

    changes in parsing

--- philo/srcs/utils.c
+++ philo/srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: dimioui <dimioui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 14:07:01 by dimioui           #+#    #+#             */
-/*   Updated: 2022/02/15 22:59:51 by dimioui          ###   ########.fr       */
+/*   Updated: 2022/02/16 11:19:29 by dimioui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,15 @@ int	ft_atoi(const char *str)
 	sign = 1;
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		{
-			if (str[i] == '-')
-				sign *= (-1);
-		}
-		i++;
-	}
+	if (str[i] < '0' || str[i] > '9')
+		return (-1);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = (result * 10) + (str[i] - '0');
 		i++;
 	}
+	if (str[i])
+		return (-1);
 	return (result * sign);
 }
 
